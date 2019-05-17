259799221ef2d67be8236bf110275c97bb467e60
jamieYeung <jamieyyw@gmail.com>
2019-05-16 22:56:28 -0400

    got value of selected radio button to equal array value and compare answer to the answer array

--- script.js
+++ script.js
@@ -5,54 +5,73 @@ const questionData = [
   // first question data
   {
     question: 'In which of the city or cities below is tap water not drinkable?', 
-    option: ['Bogota','Medellin','Cartagena','All of the above']
+    option: ['Bogota','Medellin','Cartagena','All of the above'],
+    answer: 'Bogota'
   },
   // second question data
   {
     question:'What is the approximate amount of Colombians that use Uber(Total Population is ~49.07 million?)', 
-    option:['1 mil, 2.3 mil, more than 50% , No one it\'s illegal']
+    option:['1 mil, 2.3 mil, more than 50% , No one it\'s illegal'],
+    answer: '2.3 mil'
+  },
+    // third question data
+  {
+    question: 'What is the approximate amount of Colombians that use Uber(Total Population is ~49.07 million?)',
+    option: ['1 mil, 2.3 mil, more than 50% , No one it\'s illegal'],
+    answer: '2.3 mil'
+  },
+    // fourth question data
+
+  {
+    question: 'What is the approximate amount of Colombians that use Uber(Total Population is ~49.07 million?)',
+    option: ['1 mil, 2.3 mil, more than 50% , No one it\'s illegal'],
+    answer: '2.3 mil'
+  },
+    // fifth question data
+
+  {
+    question: 'What is the approximate amount of Colombians that use Uber(Total Population is ~49.07 million?)',
+    option: ['1 mil, 2.3 mil, more than 50% , No one it\'s illegal'],
+    answer: '2.3 mil'
   }
 ]
-// const answerData = [
-//   // first answer
-//   {
-
-//   }
-// ]
-
-  // third question data
+const quizQuestion = questionData[0].question;
   // fourth question data
   // fifth question data
-
 // 1st step
 //insert the option from questionData value to the form as radio buttons from the array
 // get the select/radio button to take the value from the questionData.option array and display on page
 // make function to create html for each options of each question
 
+// for loop for making each question
+// start of for loop**********
+// questionData.forEach((qLoop) =>{
+    
 
 // create question to input on page
-const quizQuestion = questionData[0].question;
-console.log(quizQuestion);
-$('.qTitle').append(quizQuestion);
+  console.log(quizQuestion);
+  $('.qTitle').append(quizQuestion);
+  // create label and input
+  // const label = $(`<label>`).text(`${questionData[0].option[1]}`);
+  // const radioInput = $(`<input type="radio">`).attr({ id: 'from', name: 'from' });
 
-// create label and input
-// const label = $(`<label>`).text(`${questionData[0].option[1]}`);
-// const radioInput = $(`<input type="radio">`).attr({ id: 'from', name: 'from' });
-questionData[0].option.forEach((choice) => {
-  $('.options').append($(`<label> for=choice`).text(choice));
-  $('.options').append($(`<input type="radio">`).attr({ id: choice, name: 'choice' }))
-});
-// how to get value of the radio button to return the aray information
-$('.choiceBox').on('submit', function(e){
-  e.preventDefault();
-  const radioValue = $('input[type="submit"]:checked').val();
-  
-  console.log(radioValue)
+  questionData[0].option.forEach((choice) => {
+    $('.options').append($(`<label> for=choice`).text(choice));
+    $('.options').append($(`<input type="radio" value="${choice}">`).attr({ id: choice, name: 'choice' }))
+  });
+  // how to get value of the radio button when selected
+  $('.choiceBox').on('submit', function(e){
+    e.preventDefault();
+    const radioValue = $('input[type="radio"]:checked').val();
 
-})
-//   const answer = $
-//   if(radioValue === )
-// })
+    if (radioValue === questionData[0].answer){
+        console.log('you picked the right option')
+      }else{
+        console.log('wrong answer')
+      }
+    })
+    // end of for loop for entire quiz*********
+  // });
 
 // 
 //   function questionData(){
@@ -63,7 +82,7 @@ $('.choiceBox').on('submit', function(e){
 
 
 
-// button function
+
 $(function(){
 
  
