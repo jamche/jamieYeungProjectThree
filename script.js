b378312821e79337ec706912d86724a9d415a629
jamieYeung <jamieyyw@gmail.com>
2019-05-17 23:13:56 -0400

    Had to redo the entire js script since error could not be found, but code now works for mvp. can do styling + fonts now

--- script.js
+++ script.js
@@ -1,106 +1,113 @@
-// data
-
-// get data of questions in
 const questionData = [
   // first question data
   {
-    question: 'In which of the city or cities below is tap water not drinkable?', 
-    option: ['Bogota','Medellin','Cartagena','All of the above'],
-    answer: 'Bogota'
+    question: 'In which of the city or cities below is tap water not drinkable?',
+    option: ['Bogota', 'Medellin', 'Cartagena', 'All of the above'],
+    answer: 'Cartagena',
+    answerText: 'Correct answer. Since Cartagena is in the coastal region of Colombia, tap water is not drinkable. In cities such Bogota & Medellin, it is drinkable as they are located within the mountainous regions, where the source of water is from.'
   },
   // second question data
   {
-    question:'What is the approximate amount of Colombians that use Uber(Total Population is ~49.07 million?)', 
-    option:['1 mil, 2.3 mil, more than 50% , No one it\'s illegal'],
-    answer: '2.3 mil'
-  },
-    // third question data
-  {
     question: 'What is the approximate amount of Colombians that use Uber(Total Population is ~49.07 million?)',
-    option: ['1 mil, 2.3 mil, more than 50% , No one it\'s illegal'],
-    answer: '2.3 mil'
+    option: ['1 mil', '2 mil', '3 mil', 'Isn\'t Uber illegal in Colombia?'],
+    answer: '2 mil',
+    answerText:'There are still about 2 million people and still increasing that use uber throughout the country even if it is illegal. Uber is still cheaper than the other options currently to travel throughout a major city. '
   },
-    // fourth question data
+  // third question data
+  {
+    question: 'Since we all need to eat at some point, what would the average cost of a typical meal be in Colombia',
+    option: ['2$ - 4$', '5$ - 9$', '10$ - 14.5$', '14.5$ - 20$', 'cheaper than 2$'],
+    answer: '5$ - 9$',
+    answerText: 'Correct! In most cities you can go in to any restaurant and order what they call a "Menu del Dia", which is a typical Colombian dish that contains rice, beans, meat/fish and vegetables.'
+  },
+  // fourth question data
 
   {
-    question: 'What is the approximate amount of Colombians that use Uber(Total Population is ~49.07 million?)',
-    option: ['1 mil, 2.3 mil, more than 50% , No one it\'s illegal'],
-    answer: '2.3 mil'
+    question: 'What would be the typical cost to fly between any major cities within Colombia',
+    option: ['5$-10$', '11$-41$', '41$-61$', '62$', 'flights are expensive yo'],
+    answer: '11$-41$',
+    answerText: 'Correct! Flights within Colombia are super cheap! Even if booked a few days beforehand, flights can cost sometimes less than $20 for a 1-2 hour flight. Which, in comparison to an 8 hour bus ride with a similar cost sounds much better.'
   },
-    // fifth question data
-
+  // fifth question
   {
-    question: 'What is the approximate amount of Colombians that use Uber(Total Population is ~49.07 million?)',
-    option: ['1 mil, 2.3 mil, more than 50% , No one it\'s illegal'],
-    answer: '2.3 mil'
+    question: 'There is a saying in Colombia that goes likes" No des papaya" which translates to "Don\'t give papaya". What does this mean? ',
+    option: ['If i bought a papaya', 'I shouldn\'t give my papaya to anyone', 'I shouldn\'t buy fruits on the street', 'Don\'t give an opportunity to be pickpocketed' ],
+    answer: 'Don\'t give an opportunity to be pickpocketed',
+    answerText: 'Correct! Colombia is safer than ever to visit. But there are still issues with pickpockets. So one should always ensure that they keep their belongings out of reach of being pickpocketed.'
   }
 ]
-const quizQuestion = questionData[0].question;
-  // fourth question data
-  // fifth question data
-// 1st step
-//insert the option from questionData value to the form as radio buttons from the array
-// get the select/radio button to take the value from the questionData.option array and display on page
-// make function to create html for each options of each question
 
-// for loop for making each question
-// start of for loop**********
-// questionData.forEach((qLoop) =>{
+// const questionData = undefined;
+let count = 0;  
+// for completed
+let j = 0;
+
+function displayQuestion(question) {
+  
+  $('.qTitle').append(question[count].question);
+  console.log(question[count].question);
+  
+  for (let i = 0; i < questionData[count].option.length; i++) {
+  
+    $('.options').append($(`<div><label> for=choice`).text(questionData[count].option[i])) +
+    console.log(question[count].option[i]);
     
+    $('.options').append($(`<input type="radio" value="${questionData[count].option[i]}" </div>`).attr({ id: 'answer', name: 'choice' }));
+  }
 
-// create question to input on page
-  console.log(quizQuestion);
-  $('.qTitle').append(quizQuestion);
-  // create label and input
-  // const label = $(`<label>`).text(`${questionData[0].option[1]}`);
-  // const radioInput = $(`<input type="radio">`).attr({ id: 'from', name: 'from' });
+}
+// submit button
+$('.choiceBox').on('submit', function (e) {
+  e.preventDefault();
+  const radioValue = $('input[type="radio"]:checked').val();
+  if (radioValue === questionData[count].answer) {
+    $('.answerBox').append($(`<p>${questionData[count].answerText}</p>`));
+    $('#next').css('visibility','visible');
+    $('input').css('visibility','hidden');
+    
+    console.log('you picked the right option')
+    // count the answer +1 if correct
+    // 
+    // keep count of questions completed
+    j++;
+    console.log(j);
+    if (j === 5) {
+    alert('You\'ve completed the quiz! Hopefully you learned a bit more about Colombia in case you end up visiting the country. Come back later for more quizzes for other countries!');
+    }
+  } else {
+    alert('Wrong choice, please select another option to continue');
+    console.log('wrong answer')
+  }
+  // $('.wrongAnswer').html('');
 
-  questionData[0].option.forEach((choice) => {
-    $('.options').append($(`<label> for=choice`).text(choice));
-    $('.options').append($(`<input type="radio" value="${choice}">`).attr({ id: choice, name: 'choice' }))
+})
+
+const next = $('#next');
+// resets the quiz & make it count up a question
+$(next).on('click', function (event) {
+  $('#next').css('visibility', 'hidden');
+  $('input').css('visibility', 'visible');
+  event.preventDefault();
+  // $('.test').empty();
+  $('.qTitle').html('');
+  $('.options').html('');
+  $('.answerBox').html('');
+  count++;
+  displayQuestion(questionData);
+  
   });
-  // how to get value of the radio button when selected
-  $('.choiceBox').on('submit', function(e){
-    e.preventDefault();
-    const radioValue = $('input[type="radio"]:checked').val();
-
-    if (radioValue === questionData[0].answer){
-        console.log('you picked the right option')
-      }else{
-        console.log('wrong answer')
-      }
-    })
-    // end of for loop for entire quiz*********
-  // });
-
-// 
-//   function questionData(){
-      
-//     }
-// 
 
 
 
+// print the first object in the array
+// append the question to the title
+// append the label and input to options
+// compare values
 
-
+// run the damn thing
 $(function(){
 
- 
-
-
+  displayQuestion(questionData);
+  
 
 });
-
-
-// 2nd step
-// make the selected choice compare vs questionData.answer
-
-
-
-
-
-// have radio button = value of the option button
-// to find value of checked or selected radio button
-// compare the choice of the user from select button  to the answer
-// if answer is correct, indicate that answer is correct and display text of information
-// add a point if choice === answer, don't add any point if choice is !== answer and ensure point is stored
